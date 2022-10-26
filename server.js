8ec7bebc21b63a085c7bdbe239d99f8e0c56dd71
Nidhal Ghdiri <ghdiri.nidhal@gmail.com>
2022-10-26 08:33:54 +0100

    update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { readdirSync } = require("fs");
 const dotenv = require("dotenv");
+const path = require("path");
 dotenv.config();
 
 const app = express();
@@ -14,6 +15,16 @@ readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
 // Connection and Inialize Database
 require("./startup/db")();
 require("./startup/prod")(app);
+
+app.use(express.static(path.join("https://hms-oman.onrender.com/", "build")));
+
+// ...
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join("https://hms-oman.onrender.com/", "build", "index.html")
+  );
+});
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
